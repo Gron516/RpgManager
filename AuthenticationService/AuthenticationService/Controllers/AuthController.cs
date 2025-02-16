3f6f2b62db4ebe3de2a9a336c4c111150727d346
gron516 <gron516@gmail.com>
2025-02-16 18:41:19 +0300

    перенес логику работы с бд в отдельный сервис

--- AuthenticationService/AuthenticationService/Controllers/AuthController.cs
+++ AuthenticationService/AuthenticationService/Controllers/AuthController.cs
@@ -12,25 +12,15 @@ public class AuthController : Controller
     [HttpPost("login")]
     public IResult Login([FromBody]Person loginData)
     {
-        // находим пользователя 
-        var person = FindinDb(new() { Email = loginData.Email, Password = loginData.Password });
+
+        var person = Service.DbService.AuthServiceFindPerson(loginData);
         // если пользователь не найден, отправляем статусный код 401
         if(person is null) return Results.Unauthorized();
-     
-        var claims = new List<Claim> {new Claim(ClaimTypes.Name, person.Email) };
-        // создаем JWT-токен
-        var jwt = new JwtSecurityToken(
-            issuer: AuthOptions.ISSUER,
-            audience: AuthOptions.AUDIENCE,
-            claims: claims,
-            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
-            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
-        var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
- 
         // формируем ответ
+        
         var response = new
         {
-            access_token = encodedJwt,
+            access_token =  Service.DbService.AuthServiceCreateToken(person),
             username = person.Email
         };
  
@@ -49,27 +39,7 @@ public class AuthController : Controller
         return Results.Json(response);
     }
 
-    public static void AddtoDb(Person person)
-    {
-        using (ApplicationContext db = new ApplicationContext())
-        {
-            db.Persons.Add(person);
-            db.SaveChanges();
-        }
-        
-    }
-    
-    public static Person FindinDb(Person person)
-    {
 
-        using (ApplicationContext db = new ApplicationContext())
-        {
-            Person wantedUseer = person;
-            var user = db.Persons.FirstOrDefault(p => p.Email == wantedUseer.Email && p.Password == wantedUseer.Password);
-            return user;
-        }
-        
-    }
 
 }
 
