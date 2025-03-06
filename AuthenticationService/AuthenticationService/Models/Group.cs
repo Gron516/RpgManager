b8d586450ba85699a65ae926ce3c086aca09063f
gron516 <gron516@gmail.com>
2025-03-06 20:57:05 +0300

    поменял id на guid. основные изменения в БД

--- AuthenticationService/AuthenticationService/Models/Group.cs
+++ AuthenticationService/AuthenticationService/Models/Group.cs
@@ -2,7 +2,7 @@
 
 public class Group
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string? System { get; set; }
     public string? Description { get; set; }
--- AuthenticationService/AuthenticationService/Repositories/IGroupsRepository.cs
+++ AuthenticationService/AuthenticationService/Repositories/IGroupsRepository.cs
@@ -5,7 +5,7 @@ namespace AuthenticationService.Repositories;
 public interface IGroupsRepository
 {
     Task Add(Group group);
-    Task<Group?> Get(int id);
+    Task<Group?> Get(Guid id);
     Task<Group[]?> GetAll();
     Task Change(Group newGroup, Group oldGroup);
     Task Delete(Group group);
